9c0e48a39a1e9b91eb2fc29212c312fe9970663f
KIRBY-PARK <pkirby0426@gmail.com>
2024-12-18 01:06:55 +0900

    🎉 docs(config) : Begin a project
    Creating a Calendar App Using JPA Upload Initial Code.

---
+++ src/main/java/com/calendar/JPA_Calendar/JpaCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.calendar.JPA_Calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calendar/JPA_Calendar/JpaCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendar.JPA_Calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
