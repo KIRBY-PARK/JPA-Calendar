f7fad90cdc4485e55b8f91053f83266333277959
KIRBY-PARK <pkirby0426@gmail.com>
2024-12-19 15:14:30 +0900

    🛠️ feat(config) : Create and link databases
    Database creation and connection completed.
    All parts that were not imported have been imported.

--- src/main/java/com/calendar/JPA_Calendar/entity/BaseEntity.java
+++ src/main/java/com/calendar/JPA_Calendar/entity/BaseEntity.java
@@ -1,6 +1,10 @@
 package com.calendar.JPA_Calendar.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
