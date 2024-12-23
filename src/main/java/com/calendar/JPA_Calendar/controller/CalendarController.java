d6f56a2ef5aba8219a3d57dd5a21369683d4139a
KIRBY-PARK <pkirby0426@gmail.com>
2024-12-23 10:07:11 +0900

    feat(auth) : Add empty initialization and destruction code You have completed the initialization and destruction operation code addition for the empty life cycle.

--- src/main/java/com/calendar/JPA_Calendar/controller/CalendarController.java
+++ src/main/java/com/calendar/JPA_Calendar/controller/CalendarController.java
@@ -2,6 +2,8 @@ package com.calendar.JPA_Calendar.controller;
 
 import com.calendar.JPA_Calendar.dto.CalendarCreateRequestDto;
 import com.calendar.JPA_Calendar.service.CalendarService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,16 @@ public class CalendarController {
     }
 
     // 기능
+    @PostConstruct
+    public void init() {
+        System.out.println("@PostConstruct: 초기화 작업 실행");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("@PreDestroy: 소멸 작업 실행");
+    }
+    
     public void createCalendarAPI(@RequestBody CalendarCreateRequestDto calendarCreateRequestDto) {
         String name = calendarCreateRequestDto.getName();
         String title = calendarCreateRequestDto.getTitle();
